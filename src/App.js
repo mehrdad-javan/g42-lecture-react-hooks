1e7ea7170672d7ba424092c21cca0f354bed9e72
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-09-21 10:46:00 +0200

    userEffect example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Person from './Person';
+import UseEffectExample from './UseEffectExample';
 
 const App = () => {
     return (
         <div>
             <Person />
+
+            <hr/>
+
+            <UseEffectExample />
         </div>
     );
 };
---
+++ src/UseEffectExample.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+const UseEffectExample = () => {
+    const [dateTime, setDateTime] = useState('Default Value: .....');
+    const [reload, setReload] = useState(false);
+
+     useEffect( ()=> {
+        console.log('useEffect has been executed!');
+        const currentDate = new Date();
+        setDateTime(currentDate.toString());
+     }  ,  [reload] );
+
+
+
+     const updateDateHandler = () => {
+            setReload(true);
+     }
+
+
+    return (
+        <div className='container'>
+            <h3>useEffect hook function example</h3>
+            <div className='alert alert-info'>{dateTime}</div>
+            <div className='row'>
+                <div className='col'>
+                    <button type='button' className='btn btn-info' onClick={updateDateHandler}>Update Date</button>
+                </div>
+            </div>
+        </div>
+    );
+
+};
+
+export default UseEffectExample;
